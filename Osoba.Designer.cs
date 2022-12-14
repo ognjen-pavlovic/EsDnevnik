7f72fe76b88472d50ef292240a5dd15f1edef84f
ognjen-pavlovic <ognjen.pavlovic@prvabeogim.edu.rs>
2022-12-14 19:19:31 +0100

    Update 1.

--- Osoba.Designer.cs
+++ Osoba.Designer.cs
@@ -53,58 +53,67 @@ namespace EsDnevnik2022A
             // tbId
             // 
             this.tbId.Enabled = false;
-            this.tbId.Location = new System.Drawing.Point(173, 34);
+            this.tbId.Location = new System.Drawing.Point(260, 52);
+            this.tbId.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbId.Name = "tbId";
-            this.tbId.Size = new System.Drawing.Size(100, 20);
+            this.tbId.Size = new System.Drawing.Size(148, 26);
             this.tbId.TabIndex = 0;
             // 
             // tbIme
             // 
-            this.tbIme.Location = new System.Drawing.Point(173, 75);
+            this.tbIme.Location = new System.Drawing.Point(260, 115);
+            this.tbIme.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbIme.Name = "tbIme";
-            this.tbIme.Size = new System.Drawing.Size(100, 20);
+            this.tbIme.Size = new System.Drawing.Size(148, 26);
             this.tbIme.TabIndex = 1;
             // 
             // tbPrezime
             // 
-            this.tbPrezime.Location = new System.Drawing.Point(173, 118);
+            this.tbPrezime.Location = new System.Drawing.Point(260, 182);
+            this.tbPrezime.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbPrezime.Name = "tbPrezime";
-            this.tbPrezime.Size = new System.Drawing.Size(100, 20);
+            this.tbPrezime.Size = new System.Drawing.Size(148, 26);
             this.tbPrezime.TabIndex = 2;
             // 
             // tbAdresa
             // 
-            this.tbAdresa.Location = new System.Drawing.Point(173, 155);
+            this.tbAdresa.Location = new System.Drawing.Point(260, 238);
+            this.tbAdresa.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbAdresa.Name = "tbAdresa";
-            this.tbAdresa.Size = new System.Drawing.Size(100, 20);
+            this.tbAdresa.Size = new System.Drawing.Size(148, 26);
             this.tbAdresa.TabIndex = 3;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(173, 195);
+            this.textBox5.Location = new System.Drawing.Point(260, 300);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.Size = new System.Drawing.Size(148, 26);
             this.textBox5.TabIndex = 4;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(173, 235);
+            this.textBox6.Location = new System.Drawing.Point(260, 362);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.Size = new System.Drawing.Size(148, 26);
             this.textBox6.TabIndex = 5;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(173, 270);
+            this.textBox7.Location = new System.Drawing.Point(260, 415);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.Size = new System.Drawing.Size(148, 26);
             this.textBox7.TabIndex = 6;
             // 
             // btFirst
             // 
-            this.btFirst.Location = new System.Drawing.Point(36, 370);
+            this.btFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btFirst.Location = new System.Drawing.Point(82, 567);
+            this.btFirst.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btFirst.Name = "btFirst";
-            this.btFirst.Size = new System.Drawing.Size(75, 23);
+            this.btFirst.Size = new System.Drawing.Size(56, 56);
             this.btFirst.TabIndex = 7;
             this.btFirst.Text = "<<";
             this.btFirst.UseVisualStyleBackColor = true;
@@ -112,9 +121,11 @@ namespace EsDnevnik2022A
             // 
             // btPrev
             // 
-            this.btPrev.Location = new System.Drawing.Point(127, 370);
+            this.btPrev.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btPrev.Location = new System.Drawing.Point(218, 567);
+            this.btPrev.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btPrev.Name = "btPrev";
-            this.btPrev.Size = new System.Drawing.Size(75, 23);
+            this.btPrev.Size = new System.Drawing.Size(56, 56);
             this.btPrev.TabIndex = 8;
             this.btPrev.Text = "<";
             this.btPrev.UseVisualStyleBackColor = true;
@@ -122,9 +133,11 @@ namespace EsDnevnik2022A
             // 
             // btNext
             // 
-            this.btNext.Location = new System.Drawing.Point(339, 370);
+            this.btNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btNext.Location = new System.Drawing.Point(536, 567);
+            this.btNext.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btNext.Name = "btNext";
-            this.btNext.Size = new System.Drawing.Size(75, 23);
+            this.btNext.Size = new System.Drawing.Size(56, 56);
             this.btNext.TabIndex = 9;
             this.btNext.Text = ">";
             this.btNext.UseVisualStyleBackColor = true;
@@ -132,9 +145,11 @@ namespace EsDnevnik2022A
             // 
             // btLast
             // 
-            this.btLast.Location = new System.Drawing.Point(434, 370);
+            this.btLast.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLast.Location = new System.Drawing.Point(679, 567);
+            this.btLast.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btLast.Name = "btLast";
-            this.btLast.Size = new System.Drawing.Size(75, 23);
+            this.btLast.Size = new System.Drawing.Size(56, 56);
             this.btLast.TabIndex = 10;
             this.btLast.Text = ">>";
             this.btLast.UseVisualStyleBackColor = true;
@@ -143,35 +158,39 @@ namespace EsDnevnik2022A
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(53, 41);
+            this.label1.Location = new System.Drawing.Point(80, 63);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.Size = new System.Drawing.Size(23, 20);
             this.label1.TabIndex = 11;
             this.label1.Text = "Id";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 81);
+            this.label2.Location = new System.Drawing.Point(84, 125);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.Size = new System.Drawing.Size(36, 20);
             this.label2.TabIndex = 12;
             this.label2.Text = "Ime";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(56, 124);
+            this.label3.Location = new System.Drawing.Point(84, 191);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.Size = new System.Drawing.Size(66, 20);
             this.label3.TabIndex = 13;
             this.label3.Text = "Prezime";
             // 
             // btUpd
             // 
-            this.btUpd.Location = new System.Drawing.Point(229, 329);
+            this.btUpd.Location = new System.Drawing.Point(344, 506);
+            this.btUpd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btUpd.Name = "btUpd";
-            this.btUpd.Size = new System.Drawing.Size(75, 23);
+            this.btUpd.Size = new System.Drawing.Size(112, 35);
             this.btUpd.TabIndex = 14;
             this.btUpd.Text = "Izmeni";
             this.btUpd.UseVisualStyleBackColor = true;
@@ -179,25 +198,28 @@ namespace EsDnevnik2022A
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 296);
+            this.textBox1.Location = new System.Drawing.Point(18, 455);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(464, 20);
+            this.textBox1.Size = new System.Drawing.Size(694, 26);
             this.textBox1.TabIndex = 15;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(326, 235);
+            this.label4.Location = new System.Drawing.Point(489, 362);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(51, 20);
             this.label4.TabIndex = 16;
             this.label4.Text = "label4";
             // 
             // btnDel
             // 
-            this.btnDel.Location = new System.Drawing.Point(339, 329);
+            this.btnDel.Location = new System.Drawing.Point(508, 506);
+            this.btnDel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnDel.Name = "btnDel";
-            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.Size = new System.Drawing.Size(112, 35);
             this.btnDel.TabIndex = 17;
             this.btnDel.Text = "Brisi";
             this.btnDel.UseVisualStyleBackColor = true;
@@ -205,9 +227,10 @@ namespace EsDnevnik2022A
             // 
             // btnIns
             // 
-            this.btnIns.Location = new System.Drawing.Point(127, 329);
+            this.btnIns.Location = new System.Drawing.Point(190, 506);
+            this.btnIns.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnIns.Name = "btnIns";
-            this.btnIns.Size = new System.Drawing.Size(75, 23);
+            this.btnIns.Size = new System.Drawing.Size(112, 35);
             this.btnIns.TabIndex = 18;
             this.btnIns.Text = "Dodaj";
             this.btnIns.UseVisualStyleBackColor = true;
@@ -215,9 +238,9 @@ namespace EsDnevnik2022A
             // 
             // Osoba
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(645, 450);
+            this.ClientSize = new System.Drawing.Size(968, 692);
             this.Controls.Add(this.btnIns);
             this.Controls.Add(this.btnDel);
             this.Controls.Add(this.label4);
@@ -237,8 +260,9 @@ namespace EsDnevnik2022A
             this.Controls.Add(this.tbPrezime);
             this.Controls.Add(this.tbIme);
             this.Controls.Add(this.tbId);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Osoba";
-            this.Text = "Form1";
+            this.Text = "EsDnevnik";
             this.Load += new System.EventHandler(this.Osoba_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
